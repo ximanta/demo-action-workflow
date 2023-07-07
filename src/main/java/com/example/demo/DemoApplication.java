3ccb81057bb787f7c7d562f67e47059a67fcc111
Simanta.Sarma <ximanta.sarma@gmail.com>
2023-07-07 16:21:12 +0530

    Added secret

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
+
+		System.out.println("I'm Running");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
